22c94600c8e27213a4a45fb0675eced18a080617
maryam sadeghi <sadeghi.mr.69@gmail.com>
2022-04-07 14:37:49 -0700

    Projects section from index page move to portial view projects

---
+++ DevCordProject/DevCard_MVC/Models/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace DevCard_MVC.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Titel { get; set; }
+        public string Descroption { get; set; }
+    }
+}
