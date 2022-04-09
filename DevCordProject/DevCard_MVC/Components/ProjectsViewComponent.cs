eef165053025f2970e0d4174d6b74b2acc3c6701
maryam sadeghi <sadeghi.mr.69@gmail.com>
2022-04-09 14:44:53 -0700

    Header and Footer move to partial view

---
+++ DevCordProject/DevCard_MVC/Components/ProjectsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.Components
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = new List<Projects>
+            {
+                new Projects(1,"Project Heading","Project intro lorem ipsum dolor sit amet...","Google","project-1.jpg"),
+                new Projects(2,"Project Heading","Project intro lorem ipsum dolor sit amet...","Dropbox","project-2.jpg"),
+                new Projects(3, "Project Heading", "Project intro lorem ipsum dolor sit amet...", "Uber", "project-3.jpg"),
+                new Projects(4, "Project Heading", "Project intro lorem ipsum dolor sit amet...", "GOT", "project-4.jpg")
+        };
+            return View("_Projects", model);
+        }
+    }
+}
--- DevCordProject/DevCard_MVC/Models/Projects.cs
+++ DevCordProject/DevCard_MVC/Models/Projects.cs
@@ -6,9 +6,19 @@ namespace DevCard_MVC.Models
 {
     public class Projects
     {
+        public Projects(long id, string name, string descroption, string client, string img)
+        {
+            Id = id;
+            Name = name;
+            Descroption = descroption;
+            Client = client;
+            Img = img;
+        }
+
         public long Id { get; set; }
         public string Name { get; set; }
         public string Descroption { get; set; }
         public string Client { get; set; }
+        public string Img { get; set; }
     }
 }
